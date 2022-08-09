88a147ad522a5d8fe99351f38648bbcbd9edf430
Ash Bars <barsegyan@topsystems.ru>
2022-08-09 18:44:32 +0300

    Updates

---
+++ precache-manifest.8102cd2ffb3624f07df88e5165a79298.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "15c25bed789bb311f78c",
+    "url": "/static/css/main.3427310b.chunk.css"
+  },
+  {
+    "revision": "15c25bed789bb311f78c",
+    "url": "/static/js/main.15c25bed.chunk.js"
+  },
+  {
+    "revision": "fb20ecc47acc3f9625f5",
+    "url": "/static/js/1.fb20ecc4.chunk.js"
+  },
+  {
+    "revision": "6110e9abf96a499d33ab",
+    "url": "/static/css/2.b854ae54.chunk.css"
+  },
+  {
+    "revision": "6110e9abf96a499d33ab",
+    "url": "/static/js/2.6110e9ab.chunk.js"
+  },
+  {
+    "revision": "4451838b954df8c0c890",
+    "url": "/static/js/runtime~main.4451838b.js"
+  },
+  {
+    "revision": "d09f619b693e54f5895b0876c50142cd",
+    "url": "/static/media/dxicons.d09f619b.woff2"
+  },
+  {
+    "revision": "ffe4b6c8dbdfcca28b24ff0cf1828473",
+    "url": "/static/media/dxicons.ffe4b6c8.ttf"
+  },
+  {
+    "revision": "e74681c1a4b2fd54332bcb113f74ad52",
+    "url": "/static/media/dxicons.e74681c1.woff"
+  },
+  {
+    "revision": "b6fd7bc84f3ac9a9e59f013d656403ac",
+    "url": "/index.html"
+  }
+];
